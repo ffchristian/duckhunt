70e8895aed60b424512d7dcb9a30934dd8e36cf2
Christian Irack <christian@irack.mx>
2016-12-03 21:50:06 -0600

    Borrador de escenario

--- src/modules/App.js
+++ src/modules/App.js
@@ -12,6 +12,10 @@ class App extends Component {
     $('#entrar').on('click',function(e){
       $('#splashpage').hide();
       $('#niveles').show();
+    });
+    $('#seleccionNivel').on('click',function(e){
+      $('#niveles').hide();
+      $('#duckHunt').show();
     })
 
   }
@@ -37,7 +41,20 @@ arma(){
     return (
       <div className="app-js">
             <div id="splashpage"><img src={startpage} /><div id="entrar">Entrar</div></div>
-            <div id="niveles">Selector de nivel</div>
+            <div id="niveles">Selector de nivel<div id="seleccionNivel">Primer nivel</div></div>
+            <div id="duckHunt">
+              <div id="ducksScore"></div>
+              <div id="globalScore"></div>
+              <div id="cieloAzul">cuelo</div>
+              <div id="pasto"></div>
+              <div id="perro"></div>
+              <div id="patos">
+                <div id="pato1"></div>
+                <div id="pato2"></div>
+                <div id="pato3"></div>
+                <div id="pato4"></div>
+              </div>
+            </div>
       			<div id="arma"></div>
       </div>
     );
