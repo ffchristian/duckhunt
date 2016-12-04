26ea86da7da104078a6a76b8bbb78265a7fb0654
Christian Irack <christian@irack.mx>
2016-12-04 00:34:32 -0600

    Experimentando con colisiones

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import jQuery from 'jquery';
 import startpage from '../assets/duck-hunt1.png';
 
+var patos = ['pato1','pato2','pato3','pato4'];
 
+
+function colision(){
+    for(var pato in patos){
+      var pato = $('#'+patos[pato]);
+      var trigger = $('#trigger');
+      var rangoX = [Math.round(pato.position().left),Math.round(pato.position().left+pato.width())];
+      var rangoY = [Math.round(pato.position().top),Math.round(pato.position().top+pato.height())];
+      var triggerY = [Math.round(trigger.position().left),Math.round(trigger.position().left+pato.width())];
+      var triggerX = [Math.round(trigger.position().top),Math.round(trigger.position().top+pato.height())];
+      if(triggerX[0]>rangoX[0] &&  triggerX[1]<rangoX[1]){
+        console.log('Pato '+pato);
+      }
+      //console.log(rangoX,rangoY);
+      //console.log(triggerY, triggerX);
+    }
+}
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +43,17 @@ class App extends Component {
       var xAdd = $('#app-js').position().left;
        var yAdd = $('#app-js').position().top;
       $("#trigger").css({left:event.clientX-xAdd-add,top:event.clientY-yAdd-add});
+    }).click(function(){
+      colision();
+      //setTimeout(function(){ console.log(patos[0]); }, 1000);
+
     });
+
+    
+    /* Comment */
+
+    
+
 
   }
 
@@ -61,10 +87,10 @@ arma(){
               <div id="pasto"></div>
               <div id="perro"></div>
               <div className="patos">
-                <div id="pato1"></div>
-                <div id="pato2"></div>
-                <div id="pato3"></div>
-                <div id="pato4"></div>
+                <div id="pato1">1</div>
+                <div id="pato2">2</div>
+                <div id="pato3">3</div>
+                <div id="pato4">4</div>
               </div>
             </div>
       			<div id="arma"></div>
