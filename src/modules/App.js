9113f2054318574c142cdd85a075046c2737c7ae
Christian Irack <christian@irack.mx>
2016-12-03 23:12:00 -0600

    Patos volando y trigger

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import jQuery from 'jquery';
 import startpage from '../assets/duck-hunt1.png';
+
+
+
 class App extends Component {
   constructor(props) {
   	super(props);
@@ -16,7 +20,14 @@ class App extends Component {
     $('#seleccionNivel').on('click',function(e){
       $('#niveles').hide();
       $('#duckHunt').show();
-    })
+    });
+    /* Alinear trigger */
+    $('#app-js').mousemove(function( event ) {
+      var add = 35;
+      var xAdd = $('#app-js').position().left;
+       var yAdd = $('#app-js').position().top;
+      $("#trigger").css({left:event.clientX-xAdd-add,top:event.clientY-yAdd-add});
+    });
 
   }
 
@@ -39,16 +50,17 @@ arma(){
 
   render() {
     return (
-      <div className="app-js">
+      <div id="app-js">
             <div id="splashpage"><img src={startpage} /><div id="entrar">Entrar</div></div>
             <div id="niveles">Selector de nivel<div id="seleccionNivel">Primer nivel</div></div>
             <div id="duckHunt">
+              <div id="trigger"></div>
               <div id="ducksScore"></div>
-              <div id="globalScore"></div>
-              <div id="cieloAzul">cuelo</div>
+              <div id="globalScore" className="fuente1">100000</div>
+              <div id="cieloAzul"></div>
               <div id="pasto"></div>
               <div id="perro"></div>
-              <div id="patos">
+              <div className="patos">
                 <div id="pato1"></div>
                 <div id="pato2"></div>
                 <div id="pato3"></div>
