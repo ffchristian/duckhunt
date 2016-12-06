da506a7e842da11a1446c9aa8ff4728fa96220ab
Christian Irack <christian@irack.mx>
2016-12-06 02:08:58 -0600

    Error de directorio

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './modules/App';
+import App from './modules/app';
 import './css/app.css';
 ReactDOM.render(<App />,document.getElementById('app'));
