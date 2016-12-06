2ba0ae0316476e0b98e700c5c5a1fb35a0571112
Christian Irack <christian@irack.mx>
2016-12-06 02:49:23 -0600

    Nombres de archivos corregidos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './modules/app';
-import './css/app.css';
+import App from './modules/App';
+import './css/App.css';
 ReactDOM.render(<App />,document.getElementById('app'));
